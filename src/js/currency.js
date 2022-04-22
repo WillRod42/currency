c64df935f7e1c102a89f152d1b832c2ac545dd18
William Rodriguez <william@rodriguez1.com>
2022-04-22 14:01:58 -0700

    fetch currency conversions

--- src/js/currency.js
+++ src/js/currency.js
@@ -10,6 +10,30 @@ export default class Currency {
         } else {
           return new Map(json["supported_codes"]);
         }
+      })
+      .catch(error => {
+        return error;
       });
   }
+
+  static getConversionRates() {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+      .then(response => {
+        return response.json();
+      })
+      .then(json => {
+        if (json.result !== "success") {
+          throw new Error(json["error-type"]);
+        } else {
+          return new Map(json["conversion_rates"]);
+        }
+      })
+      .catch(error => {
+        return error;
+      });
+  }
+
+  // static convertCurrency(amount, inType, outType) {
+
+  // }
 }
