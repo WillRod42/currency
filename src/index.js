d338ef9871d48fd11e2a129dce2b51085713e6cd
William Rodriguez <william@rodriguez1.com>
2022-04-22 11:58:50 -0700

    Remove autocomplete list when input loses focus

--- src/index.js
+++ src/index.js
@@ -9,17 +9,23 @@ async function loadAutoComplete() {
   let currencies = await Currency.getCurrencies();
   $(".currency").on("input", function() {
     let text = $(this).val();
-    let listHTML = `<ul>`;
-    currencies.forEach(function(name) {
-      if (name.substring(0, text.length).toLowerCase() === text.toLowerCase()) {
-        listHTML += `<li><strong>${name.substring(0, text.length)}</strong>${name.substring(text.length)}</li>`;
-      }
-    });
-    $(this).siblings("ul").remove();
-    $(this).parent().append(listHTML + "</ul>");
+    if (text !== "") {
+      let listHTML = `<ul>`;
+      currencies.forEach(function(name) {
+        if (name.substring(0, text.length).toLowerCase() === text.toLowerCase()) {
+          listHTML += `<li><strong>${name.substring(0, text.length)}</strong>${name.substring(text.length)}</li>`;
+        }
+      });
+      $(this).siblings("ul").remove();
+      $(this).parent().append(listHTML + "</ul>");
+    }
   });
 }
 
 $(function() {
   loadAutoComplete();
+  $(".currency").on("blur", function() {
+    console.log("test");
+    $(this).siblings("ul").remove();
+  });
 });
