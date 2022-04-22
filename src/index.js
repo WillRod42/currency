8bca5cca5ef5556431f72c936856dca70535ded7
William Rodriguez <william@rodriguez1.com>
2022-04-22 14:21:27 -0700

    Display types of currency user selected to convert when submitting

--- src/index.js
+++ src/index.js
@@ -15,8 +15,23 @@ async function loadAutoComplete() {
   $("datalist").html(listHTML);
 }
 
+async function addFormSubmit() {
+  $("form").on("submit", function(e) {
+    e.preventDefault();
+    let inputType = getCodeFromType($("#input-type").val());
+    let outputType = getCodeFromType($("#output-type").val());
+    let convertedAmount = 0;
+    $("#output").html(`<h2>${inputType} to ${outputType}</h2><h4>${convertedAmount} ${outputType}</h4>`);
+  });
+}
+
+function getCodeFromType(type) {
+  return type.split("(")[1].substring(0, 3);
+}
+
 $(function() {
   loadAutoComplete();
+  addFormSubmit();
   $("#amount").on("change", function() {
     let input = $(this);
     input.val(parseFloat(input.val()).toFixed(2));
